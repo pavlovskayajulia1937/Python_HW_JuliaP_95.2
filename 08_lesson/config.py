4e4ec22ff97b954520225334e1d5b276263e14db
pavlovskayajulia1937 <pavlovskayajulia1937@gmail.com>
2025-07-27 13:56:28 +0300

    Delete 08_lesson directory

--- 08_lesson/config.py
+++
@@ -1,8 +0,0 @@
-API_BASE_URL = "https://ru.yougile.com/api-v2/"
-MY_CREDS = {
-            "login": 'подставьте почту',
-            "password": 'подставьте пароль',
-            "companyId": "9b039db4-a4bc-4048-af24-6774068792aa"
-}
-ADMIN = {"b0d42561-a3b0-4109-b280-0d92ed1e1430": "admin"}
-WORKER = {"b0d42561-a3b0-4109-b280-0d92ed1e1430": "worker"}
